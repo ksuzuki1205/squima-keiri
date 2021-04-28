a1acce21c2d74ed846465416d561e46604654b74
ksuzuki1205 <ksuzuki0105ff@gmail.com>
2021-04-28 17:21:09 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  validates :title,       presence: true
+  validates :partner,     presence: true
+  validates :explanation, presence: true
+  validates :price,       presence: true
 end
