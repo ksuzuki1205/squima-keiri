bd38cccdbb678aab949659ea35610de2b6c0d171
ksuzuki1205 <ksuzuki0105ff@gmail.com>
2021-05-02 08:32:13 +0900

    ログアウト状態のユーザーの機能制限の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :show, :destroy, :update]
-
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.includes(:user)
@@ -37,6 +37,12 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
